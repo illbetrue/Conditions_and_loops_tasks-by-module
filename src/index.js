a827d31361993b4a162e32575cf4eb47cc0986d5
Maksym Khomenko <Maksym_Khomenko@epam.com>
2024-02-03 20:29:41 +0200

    conditions_and_loops_tasks

--- src/index.js
+++ src/index.js
@@ -11,15 +11,19 @@
  function taskA(x, y) {
   let z1;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  if (y < 10) {
+    z1 = x + y;
+  } else {
+    z1 = x * y;
+  }
   //AND THIS ONE:
   return z1;
 }
 
 function taskB(x, y) {
-  let z2;
+  
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+    let z2 = (y < 10) ? (x + y) : (x * y);
   //AND THIS ONE:
   return z2;
 }
@@ -35,9 +39,25 @@ function taskB(x, y) {
 function calc(operator, alpha, beta) {
   let task2;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+     switch (operator) {
+       case "add":
+        task2 = alpha + beta;
+        break;
+       case "subtract":
+        task2 = alpha - beta;
+        break;
+       case "multiply":
+        task2 = alpha * beta;
+        break;
+        case "divide":
+        task2 = alpha / beta;
+        break;
+        default:
+        task2 = "Invalid operator";
+      }
   //AND THIS ONE:
   return task2;
+
 }
 
 /**
@@ -55,7 +75,17 @@ function calc(operator, alpha, beta) {
 function calculateDiscount(ransom) {
   let discount;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  if (ransom >= 0 && ransom <= 350) {
+      discount = 0;
+    } else if (ransom <= 1350) {
+      discount = 15;
+    } else if (ransom <= 2700) {
+      discount = 30;
+    } else if (ransom <= 6500) {
+      discount = 45;
+    } else {
+      discount = "Invalid ransom amount";
+    }
   //AND THIS ONE:
   return discount;
 }
@@ -71,27 +101,65 @@ function calculateDiscount(ransom) {
  */
  let forFactorial, whileFactorial, doFactorial;
  const n = 10; // 10! = 3628800
+
+ // For Loop
+  let forResult = 1;
+  for (let i = 2; i <= n; i++) {
+      forResult *= i;
+  }
+  forFactorial = forResult;
+
+  // While Loop
+  let whileResult = 1;
+  let whileIterator = 2;
+  while (whileIterator <= n) {
+      whileResult *= whileIterator;
+      whileIterator++;
+  }
+  whileFactorial = whileResult;
+
+  // Do-While Loop
+  let doResult = 1;
+  let doIterator = 2;
+  do {
+      doResult *= doIterator;
+      doIterator++;
+  } while (doIterator <= n);
+  doFactorial = doResult;
+
  
- /** Task: 2
+/** Task: 2
   * return the concatenated string from an array of substring
   * assign the result to variable 'str'
   * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
   */
  let str;
  const substr = ['I', ' love', ' JS'];
+ let result = '';
+  for (const sub of substr) {
+    result += sub;
+  }
+  str = result;
+
  
- /** Task: 3
+/** Task: 3
   * calculate a total of income of certain person
   * assign the result to the variable 'totalIncome'
   * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
   */
- let totalIncome;
- const personIncomes = {
-   salary: 1985,
-   rent: -600,
-   interestOnDeposit: 250,
-   otherExpences: -300,
- };
+  const personIncomes = {
+  salary: 1985,
+  rent: -600,
+  interestOnDeposit: 250,
+  otherExpences: -300
+  };
+
+  let sum = 0;
+  for (const key in personIncomes) {
+   sum += personIncomes[key];
+  }
+  let totalIncome = sum;
+
 
 module.exports = {
   taskA,
